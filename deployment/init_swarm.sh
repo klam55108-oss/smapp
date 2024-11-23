7f4e29b9feb413b57581eddeddaf107af6350576
klam55108-oss <klam55108@gmail.com>
2024-11-23 22:53:41 +0100

    Deployment script fixes and improvements

--- deployment/init_swarm.sh
+++ deployment/init_swarm.sh
@@ -1,15 +1,16 @@
 # Works for Amazon Linux machines
 
 # Executed on remote machine
-# Usage: cd smapp && source init_swarm.sh
+
+# Usage: cd smapp && source ./deployment/init_swarm.sh
 
 docker swarm init
 
-openssl rand -base64 32 | docker secret create mysql_password -
+openssl rand -base64 32  | tr -d '\n' | docker secret create mysql_password -
 
 temp_key_file=$(mktemp)
 chmod 600 $temp_key_file
 openssl genpkey -algorithm RSA -out $temp_key_file -pkeyopt rsa_keygen_bits:2048
 docker secret create jwt_private_key $temp_key_file
 export JWT_PUBLIC_KEY=$(echo $jwt_private_key | openssl rsa -pubout -in $temp_key_file)
-shred -u $temp_key_file
+shred -u $temp_key_file || rm $temp_key_file
--- deployment/install.sh
+++ deployment/install.sh
@@ -1,6 +1,6 @@
 # Works for Amazon Linux machines
 
-# Usage: ./deployment.install.sh <IP_ADDRESS> <PATH_TO_KEY_FILE>
+# Usage: ./deployment/install.sh <IP_ADDRESS> <PATH_TO_KEY_FILE>
 
 ip=$1
 key_file=$2
@@ -14,8 +14,8 @@ EOF
 
 ssh -i $key_file ec2-user@$ip << EOF
 # Install gomplate
-sudo curl -o /usr/local/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/latest/download/gomplate_linux-amd64
-sudo chmod 755 /usr/local/bin/gomplate
+sudo curl -o /usr/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/latest/download/gomplate_linux-amd64
+sudo chmod 755 /usr/bin/gomplate
 
 sudo yum update -y
 sudo yum install git -y
@@ -23,4 +23,12 @@ sudo yum install git -y
 git clone https://github.com/avoronoi/smapp
 cd smapp
 DEPLOY=1 gomplate -f docker-compose.yml.tmpl -o docker-compose.yml
+chmod +x deployment/init_swarm.sh
+chmod +x deployment/deploy.sh
+
+sudo curl -Lo /usr/bin/docker-credential-ecr-login https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/0.9.0/linux-amd64/docker-credential-ecr-login
+sudo chmod +x /usr/bin/docker-credential-ecr-login
+mkdir ~/.docker && echo '{
+  "credsStore": "ecr-login"
+}' > ~/.docker/config.json
 EOF
