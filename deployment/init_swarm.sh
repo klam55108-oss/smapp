0bfb9211f9585fed3f18e03d1bb7a710eafe7972
klam55108-oss <klam55108@gmail.com>
2024-11-25 23:35:12 +0100

    Use Swarm configs to store public JWT key
    - Replace environment variables with Swarm configs for storing the
      public JWT key to simplify key distribution across EC2 instances.
    - Use a custom fork of github.com/traefik-plugins/traefik-jwt-plugin to
      enable passing files with public keys, instead of keys themselves.
    - Use variable to store temporary private key instead of temporary
      file in deployment/init_swarm.sh.

--- deployment/init_swarm.sh
+++ deployment/init_swarm.sh
@@ -2,15 +2,14 @@
 
 # Executed on remote machine
 
-# Usage: cd smapp && source ./deployment/init_swarm.sh
+# Usage: cd smapp && ./deployment/init_swarm.sh
 
 docker swarm init
 
 openssl rand -base64 32  | tr -d '\n' | docker secret create mysql_password -
 
-temp_key_file=$(mktemp)
-chmod 600 $temp_key_file
-openssl genpkey -algorithm RSA -out $temp_key_file -pkeyopt rsa_keygen_bits:2048
-docker secret create jwt_private_key $temp_key_file
-export JWT_PUBLIC_KEY=$(echo $jwt_private_key | openssl rsa -pubout -in $temp_key_file)
-shred -u $temp_key_file || rm $temp_key_file
+(
+    jwt_private_key=$(openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048)
+    echo "$jwt_private_key" | docker secret create jwt_private_key -
+    echo "$jwt_private_key" | openssl rsa -pubout | docker config create jwt_public_key -
+)
